d82c2886b1a55970a7bdf65da7f9e107e2ad155c
Weidaicheng <wdcdavyc@outlook.com>
2018-07-14 20:59:17 +0800

    客户端认证

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Refit;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foo();
+
+            Console.ReadKey();
+        }
+
+        private static async void foo()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            var identityApi = RestService.For<IIdentityApi>("http://localhost:5001");
+            var result = await identityApi.Identity($"Bearer {tokenResponse.AccessToken}");
+
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+        }
+    }
+}
