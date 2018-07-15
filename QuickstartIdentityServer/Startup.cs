bad370b159646dbcae3402b738a28e6a2af9d726
Weidaicheng <wdcdavyc@outlook.com>
2018-07-15 10:15:53 +0800

    使用OpenID Connect添加用户认证

--- QuickstartIdentityServer/Startup.cs
+++ QuickstartIdentityServer/Startup.cs
@@ -15,11 +15,14 @@ namespace QuickstartIdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,10 +35,8 @@ namespace QuickstartIdentityServer
 
             app.UseIdentityServer();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
